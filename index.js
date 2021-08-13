1fe48ec1a9e3aac6d11efdd55ae38a7e80f8040f
Jack Bott <millerbott@gmail.com>
2021-08-13 14:15:04 -0400

    remove endpoint

--- index.js
+++ index.js
@@ -7,31 +7,6 @@ const io = require('socket.io')(server);
 
 app.use(express.static(__dirname));
 
-// app.get('/takePics', function(req, res) {
-//   console.log('take pictures');
-//   // console.log(req.query);
-//
-//   var command = spawn(__dirname + '/capture.bash');
-//   var output  = [];
-//
-//   command.stdout.on('data', function(chunk) {
-//     // console.log('stdout: ' + chunk);
-//     if (chunk == 'refresh\n') {
-//       console.log('refresh now');
-//       io.to('main').emit('refresh');
-//     }
-//     output.push(chunk);
-//   });
-//
-//   command.on('close', function(code) {
-//     if (code === 0)
-//       res.send(Buffer.concat(output));
-//     else
-//       res.send(500); // when the script fails, generate a Server Error HTTP response
-//   });
-// });
-
-
 io.on("connection", (socket) => {
   socket.join('main');
   socket.on("takePics", (arg) => {
